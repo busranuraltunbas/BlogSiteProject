818881810acb789a3e481fd4cc43ac1865f64314
Büşra Nur Korkut <altunbasbusranur@gmail.com>
2022-05-10 23:55:40 +0300

    booksite template created

--- routes/site.js
+++ routes/site.js
@@ -9,21 +9,72 @@ router.get('/', async (req,res) =>{
     const mySite = await BlogSite.find({}).lean()
 
     res.render('index', {
-        title: 'Todo List',
+        title: 'Books List',
         isIndex:  true,
         mySite
     })
 })
 
 
-router.get('/create', (req, res) =>{
-    res.render('Create List', {
-        title: "Todo List",
-        isCreate: true
+router.get('/library', (req, res) =>{
+    res.render('Library List', {
+        title: "Books List",
+        isLibrary: true
     })
 })
 
-router.post('/create', async (req,res) =>{
+router.post('/library', async (req,res) =>{
+    const site = new BlogSite({
+        title: req.body.title,
+    })
+    await site.save()
+
+    res.redirect('/')
+})
+
+
+
+router.get('/technology', (req, res) =>{
+    res.render('Technology List', {
+        title: "Books List",
+        isTechnology: true
+    })
+})
+
+router.post('/technology', async (req,res) =>{
+    const site = new BlogSite({
+        title: req.body.title,
+    })
+    await site.save()
+
+    res.redirect('/')
+})
+
+
+router.get('/toy', (req, res) =>{
+    res.render('Toy List', {
+        title: "Books List",
+        isToy: true
+    })
+})
+
+router.post('/toy', async (req,res) =>{
+    const site = new BlogSite({
+        title: req.body.title,
+    })
+    await site.save()
+
+    res.redirect('/')
+})
+
+router.get('/electronic', (req, res) =>{
+    res.render('Electronic List', {
+        title: "Books List",
+        isElectronic: true
+    })
+})
+
+router.post('/electronic', async (req,res) =>{
     const site = new BlogSite({
         title: req.body.title,
     })
